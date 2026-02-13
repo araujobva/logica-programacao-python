99a107f1b985f235178834b9a9852265ce018382
Bruno Araujo <araujobva@gmail.com>
2026-02-12 22:10:51 -0300

    Encontrando a área de um círculo

---
+++ EstruturasDeDados/EstruturaLinear/area_circulo.py
@@ -0,0 +1,8 @@
+# Problema: Faça um programa que calcule e mostre a área de um círculo. Sabe-se que: Area = Pi * Raio(Ao quadrado).
+    
+pi = 3.1415
+raio = input("Digite o raio do círculo >> ")
+raio = float(raio)
+area = pi * raio * raio
+print("A área do círculo é de ",area)
+
