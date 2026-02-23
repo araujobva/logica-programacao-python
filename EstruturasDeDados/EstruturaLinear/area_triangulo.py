b7c5f0d0e9af50cb5d8cbbd28f43742b560d6c3c
Bruno Araujo <araujobva@gmail.com>
2026-02-23 13:16:21 -0300

    Calculando a area de um triângulo

---
+++ EstruturasDeDados/EstruturaLinear/area_triangulo.py
@@ -0,0 +1,12 @@
+#Problema: Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Area = (base * altura) / 2.
+
+base = input("Digite a base do triângulo >> ")
+base = float(base)
+altura = input("Digite a altura do triângulo >> ")
+altura = float(altura)
+area = (base * altura) / 2
+print("A área do triângulo é ", area)
+
+
+
+
